a20516936f8b010e7723c090882f92de7c958987
fmmdevs <fmm.devs@gmail.com>
2024-05-25 11:28:00 +0200

    for deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/scss/_variables.scss";`
+      }
+    }
+  },
+  // Por defecto carga /, como estamos en un hosting y el path no va a ser /
+  // usamos una variable de entorno.
+  // Si tenemos una variable de entorno llamado PUBLIC_HTML la usamos
+  // Si no usamos "/"
+  // En la raiz del proyecto creamos archivo .env, donde van a estar las variables de entorno
+
+  publicPath: process.env.PUBLIC_HTML || "/"
 })
+
