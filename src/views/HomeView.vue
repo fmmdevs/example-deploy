8d662e1eef5e912bf193ec07f8902f21fc54c215
fmmdevs <fmm.devs@gmail.com>
2024-05-25 11:49:41 +0200

    changed title home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your first deploy in Netlify wif Vue" />
   </div>
 </template>
 
